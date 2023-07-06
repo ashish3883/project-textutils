6cab9426f635343b03a984e8b79e72b8d1a35189
Ashish <ashishkumar3883@gmail.com>
2023-07-07 01:11:11 +0530

    textbox button updated

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -34,18 +34,18 @@ export default function Textform(props) {
                 <textarea className="form-control" value={text} onChange={handelOnChange} style={{color:props.mode==='light'?'black':'white', backgroundColor:props.mode==='light'?'white':'#333333'}} id="myBox" rows="8" placeholder='Enter Text Here'></textarea>
             </div>
             
-            <button className='btn btn-primary mx-1' onClick={handelUpClick} >Convert to Uppercase</button>
-            <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to Lowercase</button>
-            <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear Text</button>
-            <button className="btn btn-primary mx-1" onClick={handleCopy}>Copy Text</button>
-            <button className="btn btn-primary mx-1" onClick={handleExtraSpace}>Remove Extra Spaces</button>
+            <button className={`btn btn-${props.mode==='dark'?'secondary':'primary'} mx-1`} onClick={handelUpClick} >Convert to Uppercase</button>
+            <button className={`btn btn-${props.mode==='dark'?'secondary':'primary'} mx-1`} onClick={handleLoClick}>Convert to Lowercase</button>
+            <button className={`btn btn-${props.mode==='dark'?'secondary':'primary'} mx-1`} onClick={handleClearClick}>Clear Text</button>
+            <button className={`btn btn-${props.mode==='dark'?'secondary':'primary'} mx-1`} onClick={handleCopy}>Copy Text</button>
+            <button className={`btn btn-${props.mode==='dark'?'secondary':'primary'} mx-1`} onClick={handleExtraSpace}>Remove Extra Spaces</button>
     </div>
     <div className="container" style={{color:props.mode==='light'?'#333333':'white'}}>
         <h2>Your Text Summary</h2>
         <p><b>{text.split(" ").length} Words & {text.length} Character</b></p>
         <p><b>{.008 * text.split(" ").length } Minutes required to read</b></p>
         <h2>Preview</h2>
-        <p>{text}</p>
+        <p>{text.length>0?text:"Enter Something in the Textbox Above to Preview it here"}</p>
     </div>
     </>
   )
